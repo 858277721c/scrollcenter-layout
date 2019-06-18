d14b649359fabeb9c8a96ea59f96c47cd05ffe9b
zhengjun <565061763@qq.com>
2019-06-18 21:45:32 +0800

    demo

--- app/src/main/java/com/sd/lib/scrollcenter_layout/demo/MainActivity.java
+++ app/src/main/java/com/sd/lib/scrollcenter_layout/demo/MainActivity.java
@@ -8,20 +8,20 @@ import com.sd.lib.sclayout.FScrollCenterLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private FScrollCenterLayout view_center_layout;
+    private FScrollCenterLayout mScrollCenterLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view_center_layout = findViewById(R.id.view_center_layout);
+        mScrollCenterLayout = findViewById(R.id.view_center_layout);
     }
 
     @Override
     public void onClick(View v)
     {
         // 移动v到中心
-        view_center_layout.focusTo(v);
+        mScrollCenterLayout.focusTo(v);
     }
 }
